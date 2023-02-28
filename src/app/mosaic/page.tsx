fc796af9a0fa36f15691dec803330257ddd8043e
Volodymyr Rusynov <volodymyr.rusynov@repairify.com>
2023-02-28 15:48:35 +0200

    *

--- src/app/mosaic/page.tsx
+++ src/app/mosaic/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <>
             <ReactColorPicker
-                onChange={() => { /*console.dir*/ }}
+                onChange={console.log}
                 initialValue={{cyan: 0, magenta: 100, yellow: 100, black: 0, alpha: 100}}
                 hasTransparency={true}
                 mode="CMYK"
